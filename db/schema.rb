ff4a0236a5fd38d961a148f97e75a311d03bac4f
codehass <hassan.elouardy06@gmail.com>
2023-05-08 14:58:09 +0000

    Migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_05_155917) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_08_143317) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reservations", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tutor_id"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"

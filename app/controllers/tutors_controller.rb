b29f89195343b865b91c5c106fc731ea4a58fb75
Leehaney gee <georgeleehaney20@gmail.com>
2023-05-08 11:44:43 +0300

    Fix linters errors

--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -1,5 +1,5 @@
 class TutorsController < ApplicationController
-  before_action :set_tutor, only: %i[ show update destroy ]
+  before_action :set_tutor, only: %i[show update destroy]
 
   # GET /tutors
   def index
@@ -39,13 +39,14 @@ class TutorsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_tutor
-      @tutor = Tutor.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def tutor_params
-      params.require(:tutor).permit(:name, :image, :speciality, :bio, :price)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_tutor
+    @tutor = Tutor.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def tutor_params
+    params.require(:tutor).permit(:name, :image, :speciality, :bio, :price)
+  end
 end
--- test/controllers/tutors_controller_test.rb
+++ test/controllers/tutors_controller_test.rb
@@ -1,35 +1,43 @@
-require "test_helper"
+require 'test_helper'
 
 class TutorsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @tutor = tutors(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get tutors_url, as: :json
     assert_response :success
   end
 
-  test "should create tutor" do
-    assert_difference("Tutor.count") do
-      post tutors_url, params: { tutor: { bio: @tutor.bio, image: @tutor.image, name: @tutor.name, price: @tutor.price, speciality: @tutor.speciality } }, as: :json
+  test 'should create tutor' do
+    assert_difference('Tutor.count') do
+      post tutors_url,
+           params: { tutor:
+            { bio: @tutor.bio, image: @tutor.image, name: @tutor.name, price: @tutor.price,
+              speciality: @tutor.speciality } },
+           as: :json
     end
 
     assert_response :created
   end
 
-  test "should show tutor" do
+  test 'should show tutor' do
     get tutor_url(@tutor), as: :json
     assert_response :success
   end
 
-  test "should update tutor" do
-    patch tutor_url(@tutor), params: { tutor: { bio: @tutor.bio, image: @tutor.image, name: @tutor.name, price: @tutor.price, speciality: @tutor.speciality } }, as: :json
+  test 'should update tutor' do
+    patch tutor_url(@tutor),
+          params: { tutor:
+            { bio: @tutor.bio, image: @tutor.image, name: @tutor.name, price: @tutor.price,
+              speciality: @tutor.speciality } },
+          as: :json
     assert_response :success
   end
 
-  test "should destroy tutor" do
-    assert_difference("Tutor.count", -1) do
+  test 'should destroy tutor' do
+    assert_difference('Tutor.count', -1) do
       delete tutor_url(@tutor), as: :json
     end
 

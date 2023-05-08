327a82eab977b647959af8ec032c97bd30ba14d7
codehass <hassan.elouardy06@gmail.com>
2023-05-08 11:54:24 +0000

    Fix linters

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy]
-  
+
   def index
     @users = User.all
     if @users
       render json: @users
     else
-      render "There is no user!"
+      render 'There is no user!'
     end
   end
 
@@ -15,7 +15,7 @@ class Api::V1::UsersController < ApplicationController
     if @user
       render json: @user
     else
-      render json: "The user you are trying to find does not exist"
+      render json: 'The user you are trying to find does not exist'
     end
   end
 
@@ -49,8 +49,8 @@ class Api::V1::UsersController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-      @user = nil
+  rescue ActiveRecord::RecordNotFound
+    @user = nil
   end
 
   # Only allow a list of trusted parameters through.

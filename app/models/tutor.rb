344a356ba5c6d2951782123c3be3d34d7f6e2c3b
Leehaney gee <georgeleehaney20@gmail.com>
2023-05-08 12:09:53 +0300

    Feat: Add validator to model

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,2 +1,9 @@
 class Tutor < ApplicationRecord
+  has_many :user
+
+  validate :name, presene: true
+  validate :image, presene: true
+  validate :speciality, presene: true
+  validate :bio, presene: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end

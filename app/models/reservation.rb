ff765e0246a983b3f38f72e9615f6d30cadec76c
codehass <hassan.elouardy06@gmail.com>
2023-05-08 18:32:56 +0000

    Add validations and relationship to reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,8 @@
 class Reservation < ApplicationRecord
+  belongs_to :user
+  belongs_to :tutor
+
+  validates :user_id, presence: true
+  validates :tutor_id, presence: true
+  validates :date, presence: true, date: { after: Date.today, message: "can't be in the past" }
 end

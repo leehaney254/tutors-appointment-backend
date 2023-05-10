a61c37cc7480a6c6fc92cfeb36fd6594335aca03
Leehaney gee <georgeleehaney20@gmail.com>
2023-05-10 16:19:20 +0300

    Feat: Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :tutors, dependent: :destroy
   has_many :reservations, dependent: :destroy
   validates :name, presence: true, length: { maximum: 50 }
